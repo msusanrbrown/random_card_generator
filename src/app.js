39650cbfbb032a9e7077f1dd5fe1ebd4121ec648
msusanrbrown <sbrow120@fau.edu>
2021-07-14 00:55:16 +0000

    FirstCommit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let top = document.querySelector(".topIcon");
+  let center = document.querySelector(".centernumber");
+  let bottom = document.querySelector(".bottomIcon");
+
+  let randsuit = Math.floor(Math.random() * 4);
+  const element = suits[randsuit];
+
+  if (element === "♥" || element === "♦") {
+    top.style.color = "red";
+    bottom.style.color = "red";
+    top.innerHTML = element;
+    bottom.innerHTML = element;
+  } else if (element === "♠" || element === "♣") {
+    top.style.color = "black";
+    bottom.style.color = "black";
+    top.innerHTML = element;
+    bottom.innerHTML = element;
+  }
+
+  let randnumber = Math.floor(Math.random() * 14);
+  const element1 = numbers[randnumber];
+  center.innerHTML = element1;
 };
+
+//write your code here
